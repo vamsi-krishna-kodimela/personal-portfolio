eb3d3453e96c8dbb79f504f8d0a4b6da1eea2fa3
Vamsi Krishna Kodimela <vamsikrishnakdml@gmail.com>
2023-06-24 18:09:52 +0530

    Scroll Working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'portfolio-1';
+
+  @ViewChild('about') about: ElementRef | undefined;
+
+  ngAfterViewInit() {
+    let offset = window.innerHeight * 4.5;
+    if (this.about) {
+      offset += this.about.nativeElement.scrollHeight;
+    }
+
+    gsap.registerPlugin(ScrollTrigger);
+    const pageAnimaionConfig: ScrollTrigger.Vars = {
+      trigger: '.main',
+      start: 'top top',
+      end: `+=${offset}`,
+      scrub: true,
+      pin: true,
+    };
+
+    const timeline = gsap.timeline({
+      scrollTrigger: pageAnimaionConfig,
+    });
+    const descriptionOffset =
+      this.about?.nativeElement.scrollHeight + 80 - window.innerHeight;
+    timeline
+      .to('#about', { top: '0', duration: 1 })
+      .to('.description', {
+        y: `-${descriptionOffset}`,
+        duration: 3,
+      })
+      .to('#projects', { top: '0', duration: 1 })
+      .to('#contactUs', { top: '0', duration: 1 });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeatherModule } from 'angular-feather';
+import {
+  Menu,
+  Github,
+  Linkedin,
+  Twitter,
+  Instagram,
+} from 'angular-feather/icons';
+
+const icons = { Menu, Github, Linkedin, Twitter, Instagram };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FeatherModule.pick(icons)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
